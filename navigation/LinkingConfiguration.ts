b98f5a4e1167be2e1ebe61a620a413ed963559c7
dike <1138832931@qq.com>
2022-07-18 22:04:26 +0800

    init

--- navigation/LinkingConfiguration.ts
+++ navigation/LinkingConfiguration.ts
@@ -4,31 +4,36 @@
  * https://reactnavigation.org/docs/configuring-links
  */
 
-import { LinkingOptions } from '@react-navigation/native';
-import * as Linking from 'expo-linking';
+import { LinkingOptions } from "@react-navigation/native";
+import * as Linking from "expo-linking";
 
-import { RootStackParamList } from '../types';
+import { RootStackParamList } from "../types";
 
 const linking: LinkingOptions<RootStackParamList> = {
-  prefixes: [Linking.makeUrl('/')],
+  prefixes: [Linking.makeUrl("/")],
   config: {
     screens: {
       Root: {
         screens: {
-          TabOne: {
+          Message: {
             screens: {
-              TabOneScreen: 'one',
+              TabOneScreen: "message",
             },
           },
-          TabTwo: {
+          Contact: {
             screens: {
-              TabTwoScreen: 'two',
+              TabTwoScreen: "contact",
+            },
+          },
+          Mine: {
+            screens: {
+              MineScreen: "mine",
             },
           },
         },
       },
-      Modal: 'modal',
-      NotFound: '*',
+      Modal: "modal",
+      NotFound: "*",
     },
   },
 };
